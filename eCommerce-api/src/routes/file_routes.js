2bff02e58ab1e3e54a2910d3cf74e0b02ae6c420
Puneet <puneet2715@gmail.com>
2022-09-28 00:28:09 +0530

    multiple files upload

--- eCommerce-api/src/routes/file_routes.js
+++ eCommerce-api/src/routes/file_routes.js
@@ -13,4 +13,18 @@ router.post("/single", upload.single("image"), async function (req, res) {
   });
 });
 
+router.post("/multiple", upload.array("images", 10), async function (req, res) {
+  const uploadedFiles = req.files;
+  if (!uploadedFiles || uploadedFiles.length == 0) {
+    return res.json({ success: false, error: "files-not-uploaded" });
+  }
+
+  var downloadUrls = [];
+  uploadedFiles.forEach(function (uploadedFile) {
+    const downloadUrl = "http://localhost:5000/" + uploadedFile.filename;
+    downloadUrls.push(downloadUrl);
+  });
+  res.json({ success: true, data: downloadUrls });
+});
+
 module.exports = router;
